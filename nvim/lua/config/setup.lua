6d8c7d5b4c20e21ba89d59a2a648d06b345eabee
William <santifon15@gmail.com>
2023-09-16 15:24:25 -0500

    add general configurations

--- nvim/lua/config/setup.lua
+++ nvim/lua/config/setup.lua
@@ -1,18 +1,34 @@
-vim.cmd.colorscheme "catppuccin"
+--Check :options for details
+-- Moving around, searching and patterns
 
-vim.opt.nu = true
-vim.opt.relativenumber = false
-
--- undotree opt
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
-
-vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-vim.opt.termguicolors = true
+--Displayin text
 
 vim.opt.scrolloff = 8
+vim.opt.wrap= true
+vim.opt.breakindent= true
+vim.opt.number = true
+vim.opt.relativenumber = false
+
+-- Syntax, highlighting and spelling
+
+vim.opt.hlsearch = true
+vim.opt.termguicolors = true
+vim.opt.spell= false
+
+-- Selecting text
+
+vim.opt.clipboard = "unnamed"
+
+-- Tabs and indenting
 
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
+vim.opt.expandtab= true
+
+vim.cmd.colorscheme "catppuccin"
+
+-- undotree opt
+--vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+--vim.opt.undofile = true
