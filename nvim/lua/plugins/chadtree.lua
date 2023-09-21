6069b838501d0989fd17d4e282126b98765e0caf
William <santifon15@gmail.com>
2023-09-21 18:54:31 -0500

    add text_colour_set option

--- nvim/lua/plugins/chadtree.lua
+++ nvim/lua/plugins/chadtree.lua
@@ -1,8 +1,17 @@
+local settings = {
+    theme = {
+      text_colour_set= 'trapdoor'
+    }
+}
+
 return {
 	'ms-jpq/chadtree',
 	branch = 'chad',
 	build = 'python3 -m chadtree deps',
 	keys = {
 		{'<leader>e', '<cmd>CHADopen<cr>'}
-	}
+	},
+  config = function()
+    --vim.api.nvim_set_var('chadtree_settings', settings)
+  end,
 }
