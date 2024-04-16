8ddd16cb1b97069030912b9b99336330101fbb6c
william <test@test.com>
2024-04-16 18:29:18 -0500

    Add fugitive, rest, rocks, treesitter plugins

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,10 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+    configs.setup({
+      ensure_installed = { "lua", "xml", "http", "json", "graphql" }
+    })
+  end
+}
