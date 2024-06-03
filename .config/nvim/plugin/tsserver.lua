067068c87a265fe80b6821ef077d367700ff5cc6
william <santifon@gmail.com>
2024-06-02 20:01:35 -0500

    update tsserver config

--- .config/nvim/plugin/tsserver.lua
+++ .config/nvim/plugin/tsserver.lua
@@ -1,31 +1,17 @@
-local function start_server()
-  local root_files = { "package.json", "tsconfig.json", "jsconfig.json", ".git" }
-  local paths = vim.fs.find(
-    root_files,
-    { upward = true, stop = vim.loop.os_homedir(), path = vim.fs.dirname(vim.api.nvim_buf_get_name(0)) }
-  )
-  local dir = vim.fs.dirname(paths[1])
-
-  if dir == nil then
-    print("Could not find root directory\n" .. paths[1])
-    return
-  end
-
-  local clientId = vim.lsp.start({
-    name = "tsserver",
-    cmd = { "typescript-language-server", "--stdio" },
-    root_dir = root_dir,
-    on_attach = function(client, bufnr)
-      print("LSP " .. client.name .. " attached to buffer " .. bufnr)
-    end
-  })
-  if (clientId ~= nil) then
-    print("lsp client id: " .. clientId .. " lsp dir: " .. dir)
-  end
-end
+local lsp = require("config.lsp")
 
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
   desc = "Start tsserver",
-  callback = start_server
+  callback = function()
+    lsp.start_server(
+      "",
+      "tsserver",
+      { "typescript-language-server", "--stdio" },
+      function(name)
+        return name:match("package.json") or name:match("tsconfig.json") or name:match("angular.json")
+      end
+    --true
+    )
+  end
 })
