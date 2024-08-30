54fde98be5bbb05104e83f9f0f28d9fb1aeef4b4
william <test@test.com>
2024-08-30 13:01:07 -0500

    Add terminal keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -12,11 +12,13 @@ vim.keymap.set("n", "<tab><tab>", ":tab split<CR>")
 vim.keymap.set("n", "<tab>c", ":tab close<CR>")
 vim.keymap.set("n", "<leader><TAB>", ":tabnext<CR>")
 vim.keymap.set("n", "<leader><S-TAB>", ":tabprevious<CR>")
+vim.keymap.set("n", "<leader>t", ":terminal<CR>", { desc = "Open terminal" })
+vim.keymap.set("t", "<leader><Esc>", "<c-\\><c-n>", { desc = "Escape terminal", noremap = true })
 
 -- Diagnostics
-vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, {desc = "Open diagnostic"})
-vim.keymap.set("n", "<leader>gd", vim.diagnostic.goto_next, {desc = "Go to next diagnostic"})
-vim.keymap.set("n", "<leader>gD", vim.diagnostic.goto_prev, {desc = "Go to prev diagnostic"})
+vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Open diagnostic" })
+vim.keymap.set("n", "<leader>gd", vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
+vim.keymap.set("n", "<leader>gD", vim.diagnostic.goto_prev, { desc = "Go to prev diagnostic" })
 
 -- LSP
 vim.keymap.set("n", "<leader>lr", lsp.lsp_restart)
