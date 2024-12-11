11ca2471123f9a9b41f5e3a20631cc8b99973218
William Fonseca <william.fonseca@micronotes.com>
2024-12-10 19:13:31 -0500

    Update codeium config

--- .config/nvim/lua/plugins/codeium.lua
+++ .config/nvim/lua/plugins/codeium.lua
@@ -1,10 +1,30 @@
 return {
   'Exafunction/codeium.vim',
   event = 'BufEnter',
-	keys = {
-    {'<leader><CR>', 'function() return vim.fn["codeium#Accept"]() end', { expr = true, silent = true }},
-    {'<c-;>', 'function() return vim.fn["codeium#CycleCompletions"](1) end', { expr = true, silent = true }},
-    {'<c-,>', 'function() return vim.fn["codeium#CycleCompletions"](-1) end', { expr = true, silent = true }},
-    {'<c-x>', 'function() return vim.fn["codeium#Clear"]() end', { expr = true, silent = true }},
-	},
+  init = function()
+    vim.g.codeium_disable_bindings = 1
+    --vim.api.nvim_call_function("codeium#GetStatusString", {}) -- Does not works!
+  end,
+  keys = {
+    { "<c-i>", function() return vim.fn['codeium#Accept']() end,             mode = "i", expr = true, silent = true },
+    { "<c-;>",  function() return vim.fn["codeium#CycleCompletions"](1) end,  mode = "i", expr = true, silent = true },
+    { "<c-,>",  function() return vim.fn["codeium#CycleCompletions"](-1) end, mode = "i", expr = true, silent = true },
+    { "<c-x>",  function() return vim.fn["codeium#Clear"]() end,              mode = "i", expr = true, silent = true },
+  },
 }
+--TODO: Check docs to enable this plugin instead.
+--return {
+--  'Exafunction/codeium.nvim',
+--  dependencies = {
+--    "nvim-lua/plenary.nvim"
+--  },
+--  event = 'BufEnter',
+--  opts = {
+--    virtual_text = {
+--      map_keys = false,
+--      key_bindings = {
+--        accept = "<c-i>"
+--      }
+--    }
+--  },
+--}
