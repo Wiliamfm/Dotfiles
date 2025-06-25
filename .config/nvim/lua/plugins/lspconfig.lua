8640e3e56d8e94ecb608cb4bd59da9c0d34d3d40
william <santifon15@gmail.com>
2025-06-25 06:39:44 -0500

    Update lspconfig plugin

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -3,18 +3,28 @@ return {
   name = "lspconfig",
   lazy = false,            -- REQUIRED: tell lazy.nvim to start this plugin at startup
   dependencies = {
-    { "williamboman/mason.nvim", config = true },
+    {
+      "williamboman/mason.nvim",
+      opts = {
+        registries = {
+
+          "github:mason-org/mason-registry",
+          "github:Crashdummyy/mason-registry",
+        }
+      },
+      config = true
+    },
     {
       "williamboman/mason-lspconfig.nvim",
       opts = {
-        ensure_installed = { "lua_ls", "gopls", "dockerls", "ts_ls" }
+        ensure_installed = { "lua_ls", "gopls", "dockerls", "ts_ls", "roslyn_ls" }
       }
     },
     -- main one
-    { "ms-jpq/coq_nvim",         branch = "coq" },
+    { "ms-jpq/coq_nvim",      branch = "coq" },
 
     -- 9000+ Snippets
-    { "ms-jpq/coq.artifacts",    branch = "artifacts" },
+    { "ms-jpq/coq.artifacts", branch = "artifacts" },
 
     -- lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty
     -- Need to **configure separately**
@@ -49,7 +59,7 @@ return {
         recommended = false
       },
       completion = {
-        skip_after = {"{", "}", "(", ")"}
+        skip_after = { "{", "}", "(", ")" }
       },
       clients = {
         lsp = {
@@ -61,23 +71,19 @@ return {
   end,
   config = function()
     -- Your LSP settings here
-    local lspconfig_ok, lspconfig = pcall(require, "lspconfig")
-    if not lspconfig_ok then
-      print("lspconfig not found")
-      return
-    end
     local coq_ok, coq = pcall(require, "coq")
     if not coq_ok then
       print("coq not found")
       return
     end
-    lspconfig.lua_ls.setup(coq.lsp_ensure_capabilities())
-    lspconfig.pyright.setup(coq.lsp_ensure_capabilities())
-    lspconfig.biome.setup(coq.lsp_ensure_capabilities())
-    lspconfig.ts_ls.setup(coq.lsp_ensure_capabilities())
-    lspconfig.ruff.setup(coq.lsp_ensure_capabilities())
-    lspconfig.gopls.setup(coq.lsp_ensure_capabilities())
-    lspconfig.dockerls.setup(coq.lsp_ensure_capabilities())
-    lspconfig.yamlls.setup(coq.lsp_ensure_capabilities())
+    vim.lsp.config("lua_ls", coq.lsp_ensure_capabilities())
+    vim.lsp.config("pyright", coq.lsp_ensure_capabilities())
+    vim.lsp.config("biome", coq.lsp_ensure_capabilities())
+    vim.lsp.config("ts_ls", coq.lsp_ensure_capabilities())
+    vim.lsp.config("ruff", coq.lsp_ensure_capabilities())
+    vim.lsp.config("gopls", coq.lsp_ensure_capabilities())
+    vim.lsp.config("dockerls", coq.lsp_ensure_capabilities())
+    vim.lsp.config("yamlls", coq.lsp_ensure_capabilities())
+    vim.lsp.config("roslyn_ls", coq.lsp_ensure_capabilities())
   end,
 }
