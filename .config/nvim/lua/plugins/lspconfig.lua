57e97c4238621cdc497413ab2d3680cbbdd92ee2
william <test@test.com>
2025-08-21 15:26:41 -0500

    Remove unused lsp configs

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -20,16 +20,5 @@ return {
         ensure_installed = { "lua_ls", "gopls", "dockerls", "ts_ls", "roslyn_ls", "harper_ls" }
       }
     },
-  },
-  config = function()
-    vim.lsp.config("lua_ls")
-    vim.lsp.config("pyright")
-    vim.lsp.config("biome")
-    vim.lsp.config("ts_ls")
-    vim.lsp.config("ruff")
-    vim.lsp.config("gopls")
-    vim.lsp.config("dockerls")
-    vim.lsp.config("yamlls")
-    vim.lsp.config("harper_ls")
-  end,
+  }
 }
